2d24d344a3fd42e87a604d67431eac29472eec36
Jeff Jensen <jjensen@apache.org>
2020-04-30 07:25:22 -0500

    keymap.c: adjust Fn layer
    * Rename to _FN_LCRM: left cursor, right mouse
    * Add mouse movements keys
    * Add cursor keys
    * Add second layer with opposite half mapping: left mouse, right cursor

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/jeffjensen/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/jeffjensen/keymap.c
@@ -5,9 +5,10 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
-#define _FN     1
-#define _NUMPAD 2
+#define _QWERTY     0
+#define _FN_LCRM    1   // left cursor, right mouse
+#define _FN_LMRC    2   // left mouse, right cursor
+#define _NUMPAD     3
 
 // Some basic macros
 #define TASK    LCTL(LSFT(KC_ESC))
@@ -20,17 +21,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_5x7(
   // left hand
     KC_EQL,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_ESC,
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           _______,
-    KC_CAPS,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           TT(_FN),
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TT(_FN_LCRM),
+    KC_CAPS,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           _______,
     OSM(MOD_LSFT),  KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,
     _______,        KC_GRV,         KC_LEFT,        KC_RGHT,
                                                                 KC_BSPC,        KC_DEL,
-                                                                KC_LCTL,        KC_HOME,
+                                                                KC_LCTL,        LT(_FN_LMRC, KC_HOME),
                                                                 KC_LALT,        KC_END,
   // right hand
     TG(_NUMPAD),    KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,
-    _______,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
-    TT(_FN),        KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOT,
+    TT(_FN_LCRM),   KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
+    _______,        KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOT,
                     KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        OSM(MOD_RSFT),
                                                     KC_UP,          KC_DOWN,        KC_LBRC,        KC_RBRC,
             KC_ENT,         KC_SPC,
@@ -38,23 +39,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_PGDN,        KC_RWIN
 ),
 
-[_FN] = LAYOUT_5x7(
+[_FN_LCRM] = LAYOUT_5x7(
   // left hand
     _______,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          _______,
-    _______,        _______,        _______,        KC_UP,          _______,        _______,        _______,
-    _______,        _______,        KC_LEFT,        KC_DOWN,        KC_RGHT,        _______,        RESET,
-    _______,        _______,        _______,        _______,        _______,        _______,
-    KC_MSTP,        KC_MPLY,        KC_MPRV,        KC_MNXT,
-                                                                _______,        _______,
+    _______,        _______,        KC_HOME,        KC_UP,          KC_END,         _______,        _______,
+    _______,        KC_PGUP,        KC_LEFT,        KC_DOWN,        KC_RGHT,        LCTL(KC_A),     _______,
+    _______,        KC_PGDN,        KC_CUT,         KC_COPY,        KC_PASTE,       _______,
+    _______,        _______,        _______,        _______,
+                                                                KC_DEL,         _______,
                                                                 _______,        _______,
                                                                 _______,        _______,
   // right hand
     _______,        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-    _______,        _______,        _______,        _______,        _______,        _______,        KC_F12,
-    _______,        _______,        _______,        _______,        _______,        _______,        _______,
-                    _______,        _______,        _______,        _______,        _______,        _______,
-                                                    _______,        _______,        _______,        _______,
-            KC_DEL,         _______,
+    _______,        _______,        KC_WH_L,        KC_MS_U,        KC_WH_R,        _______,        KC_F12,
+    _______,        _______,        KC_MS_L,        KC_MS_D,        KC_MS_R,        _______,        _______,
+                    _______,        KC_BTN1,        KC_BTN2,        KC_BTN3,        _______,        _______,
+                                                    KC_WH_D,        KC_WH_U,        _______,        _______,
+            _______,        _______,
+            _______,        _______,
+            _______,        _______
+),
+
+[_FN_LMRC] = LAYOUT_5x7(
+  // left hand
+    _______,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          _______,
+    _______,        _______,        KC_WH_L,        KC_MS_U,        KC_WH_R,        _______,        _______,
+    _______,        _______,        KC_MS_L,        KC_MS_D,        KC_MS_R,        _______,        _______,
+    _______,        _______,        KC_BTN1,        KC_BTN2,        KC_BTN3,        _______,
+    _______,        _______,        KC_WH_U,        KC_WH_D,
+                                                                KC_DEL,         _______,
+                                                                _______,        _______,
+                                                                _______,        _______,
+  // right hand
+    _______,        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
+    _______,        _______,        KC_HOME,        KC_UP,          KC_END,         _______,        KC_F12,
+    _______,        LCTL(KC_A),     KC_LEFT,        KC_DOWN,        KC_RGHT,        KC_PGUP,        _______,
+                    _______,        KC_CUT,         KC_COPY,        KC_PASTE,       KC_PGDN,        _______,
+                                                    KC_WH_U,        KC_WH_D,        _______,        _______,
+            _______,        _______,
             _______,        _______,
             _______,        _______
 ),
